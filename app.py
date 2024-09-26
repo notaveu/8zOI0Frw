2b46290f1484197d18a4ee16150cbda1f9421245
So99 <>
2024-09-26 19:51:44 -0300

    Adicionar dataaccess

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
+import os
 from flask import Flask, render_template
+from dataaccess import create_db, Task
+
+if not os.getenv('SQLALCHEMY_DATABASE_URI'):
+    from dotenv import load_dotenv
+    load_dotenv()
 
 app = Flask(__name__)
+dao = create_db(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html', message='hello world')
+    tasks = list(dao.get_all())
+    return render_template('index.html', tasks=tasks)
---
+++ dataaccess.py
@@ -0,0 +1,44 @@
+import os
+import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, registry
+from typing_extensions import Annotated
+
+str_100 = Annotated[str, 100]
+
+class Base(DeclarativeBase):
+    registry = registry(
+        type_annotation_map={
+            str_100: String(100)
+        }
+    )
+
+db = SQLAlchemy(model_class=Base)
+
+class Task(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str_100] = mapped_column()
+    body: Mapped[str_100] = mapped_column(nullable=True)
+
+class DbAccess:
+    def get_all(self):
+        return db.session.scalars(db.select(Task)).all()
+
+    def create(self, task):
+        db.session.add(task)
+        db.session.commit()
+
+    def update(self, task):
+        db.session.commit()
+
+    def delete(self, task):
+        db.session.delete(task)
+        db.session.commit()
+
+def create_db(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    return DbAccess()
